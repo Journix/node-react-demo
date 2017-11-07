d33112ab24172a9d7ee4c2768a9ff4c6b4c900a2
tan_l <tan_l@Ctrip.com>
2017-11-07 13:31:42 +0800

    init react

---
+++ static/README.md
@@ -0,0 +1,31 @@
+## 目录结构
+- src: 源代码目录
+    - pages: 页面级组件
+
+
+
+## package.json
+
+#### devDependencies
+- webpack: 打包工具选择webpack
+- babel: es6 -> es5
+- babel-loader: es6 -> es5
+- css-loader: 将编译好的css传递给下一步骤
+- style-loader: 将最终样式引入到生成的代码中
+- url-loader: Loads files as `base64` encoded URL
+- file-loader: 解析项目中的url，使之指向正确的地址
+- http-server: 基于nodejs的http服务器
+- open: Open a file or url in the user's preferred application.
+- webpack-dev-server: 一个小型的node.js Express服务器
+- html-webpack-plugin: 生成html文件
+
+#### dependencies
+- react: react核心依赖
+- react-dom: react生成DOM节点必备依赖
+- react-router-dom: react-router核心依赖
+- react-redux: react 专用 redux 库
+- react-router-redux: router 与 redux 的连接器
+- classnames: 快速生成react className
+- lodash: 工具库
+
+
---
+++ static/src/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import Index from './pages/index/index';
+import List from './pages/list/list';
+import registerServiceWorker from './registerServiceWorker';
+
+
+import { HashRouter, Route, Link, Switch } from 'react-router-dom';
+
+// 需要服务端做配置
+// import createHistory from 'history/createBrowserHistory';
+// const history = createHistory();
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+        {this.props.children}
+
+      </div>
+    );
+  }
+}
+
+const About = () => (
+  <div>
+    <h3>About</h3>
+  </div>
+)
+
+const Home = () => (
+  <div>
+    <h3>Home</h3>
+  </div>
+)
+
+const Message = ({ match }) => (
+  <div>
+    <h3>new messages</h3>
+    <h3>{match.params.id}</h3>
+  </div>
+)
+
+const Inbox = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <Route path={`${match.url}/messages/:id`} component={Message}/>
+
+  </div>
+)
+
+ReactDOM.render(
+  (<HashRouter>
+    <App>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/inbox" component={Inbox} />
+    </App>
+  </HashRouter>),
+  document.getElementById('root')
+);
+
+
+// ReactDOM.render(
+//     <Router history={history}>
+//       <div>
+//         <Route path="/" exact component={Index} />
+//         <Route exact path='aaa/:urlPathParams' component={App}/>
+//       </div>
+//     </Router>,
+//   document.getElementById('root')
+// );
+
+
+
