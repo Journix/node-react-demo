910233ec2076b0451564334c4214d22cda8938d6
Mark24 <mark.zhangyoung@qq.com>
2017-11-10 17:26:30 +0800

    添加dist目录清理插件

--- jelly/build/webpack.prd.conf.js
+++ jelly/build/webpack.prd.conf.js
@@ -1,8 +1,16 @@
+const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
-const merge = require('webpack-merge')
+const merge = require('webpack-merge');
 
 
 module.exports = merge(baseWebpackConfig, {
-
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    plugins: [
+        new CleanWebpackPlugin(['../dist'], { allowExternal: true })
+    ]
 });
