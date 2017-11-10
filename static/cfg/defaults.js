1fb6bb2259e835c5d60a6ae56aa01335b15b2869
tanling <490472172@qq.com>
2017-11-10 10:20:45 +0800

    try

--- static/cfg/defaults.js
+++ static/cfg/defaults.js
@@ -58,7 +58,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: '/assets/',
+  publicPath: '/build/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
