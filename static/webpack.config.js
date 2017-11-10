ff2a989f01e1a79d5908c141bf14d7e912d22efb
tanling <490472172@qq.com>
2017-11-10 15:10:40 +0800

    try

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const args = require('minimist')(process.argv.slice(2));
 
 // List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test', 'render', 'local'];
+const allowedEnvs = ['dev', 'dist', 'test', 'render'];
 
 // Set the correct environment
 let env;
