596eb91fe55c1c3143c5846a1ab577031618acab
Mark24Code <mark.zhangyoung@qq.com>
2017-11-11 21:30:24 +0800

    webpack.dev开发完成

--- jelly/build/webpack.prd.conf.js
+++ jelly/build/webpack.prd.conf.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const baseWebpackConfig = require('./webpack.base.conf');
 const merge = require('webpack-merge');
 
 
 module.exports = merge(baseWebpackConfig, {
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
         new webpack.optimize.UglifyJsPlugin(),
-        new CleanWebpackPlugin(['../dist/*'], { allowExternal: true })
+        new CleanWebpackPlugin(['../dist/*'], { allowExternal: true }),
+        new HtmlWebpackPlugin({ template: './index.html' })
     ]
 });
