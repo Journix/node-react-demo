bce7ebab49384e4b099dccae69076fb3fa5c6e37
tanling <490472172@qq.com>
2017-11-12 21:03:11 +0800

    remove something

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -3,29 +3,14 @@
 const path = require('path');
 const args = require('minimist')(process.argv.slice(2));
 
-// List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test', 'render'];
-
-// Set the correct environment
-let env;
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
-}
-process.env.REACT_WEBPACK_ENV = env;
+process.env.REACT_WEBPACK_ENV = args.env || "dist";
 
 /**
  * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
  * @return {Object} Webpack config
  */
-function buildConfig(wantedEnv) {
-  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
-  let validEnv = isValid ? wantedEnv : 'dev';
-  let config = require(path.join(__dirname, 'cfg/' + validEnv));
+function buildConfig(env = 'dev') {
+  let config = require(path.join(__dirname, 'cfg/' + env));
   return config;
 }
 
